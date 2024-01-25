7913f3ab3298b9d57b922152a8a1e397bee2d33f
Kimhyunjin4455 <dp4455@naver.com>
2024-01-25 23:14:30 +0900

    List인터페이스 다형성 간단 실습1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ import memoEx.ex.GenericEx;
 import memoEx.MemoMain;
 import memoEx.ex.StringEX;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -35,6 +39,65 @@ public class Main {
         GenericEx<StringEX> genericEx = new GenericEx<>();
         genericEx.putData(new StringEX());
         System.out.println(genericEx.getData());
+
+        ArrayList<String> text = new ArrayList<>();
+        text.add("김");
+        text.add("현");
+        text.add("진");
+
+        text.set(1,"이");
+        System.out.println(text);
+        System.out.println(text.get(2));
+        //text.remove(2);//text.remove("진");
+        // ArrayList는 배열 기반이므로 value보다 index로 찾아 제거하는 것이 속도면에서 효율적
+
+        // ArrayList의 논리적 공간 늘리기
+        for (int i = 0; i < 20; i++) {
+            // null 값으로 채운다
+            text.add(null);
+        }
+
+        int size = text.size();
+        System.out.println(size);
+
+
+        // LinkedList는 노드끼리의 참조값을 가지고 있는 이중 연결 리스트
+        // 데이터 삽입/삭제가 빠르지만, 특정 데이터 찾기엔 비효율적(접근보다 삽입/삭제 많을 경우에 유용)
+        // 특정 데이터 찾을 땐 최초 생성 노드부터 차례대로 검색하기 때문.
+        // 당연하지만 데이터가 많을수록 삭제는 시간이 더 소요됨
+
+        LinkedList<String> texts = new LinkedList<>();
+
+        texts.add("딸기");
+        texts.add("포도");
+        texts.add("사과");
+        // 딸기 -> 포도 -> 사과
+
+        // 첫 번째 Node의 앞에 삽입
+        texts.addFirst("시작");
+        // 시작 -> 딸기 -> 포도 -> 사과
+
+        // 마지막 Node의 뒤에 삽입
+        texts.addLast("마지막");
+        // 시작 -> 딸기 -> 포도 -> 사과 -> 마지막
+
+        texts.add(1, "중간!");
+        // 딸기 -> 중간! -> 포도 -> 사과
+
+        String text2 = texts.get(1);
+        // 포도
+
+        // 인덱스를 이용한 값 삭제
+        texts.remove(1);
+
+        // 값을 검색하여 값 삭제
+        texts.remove("중간!");
+
+        // 맨 앞의 값을 삭제
+        texts.removeFirst();
+
+        // 맨 뒤의 값을 삭제
+        texts.removeLast();
 
 
     }
--- src/memoEx/entity/Post.java
+++ src/memoEx/entity/Post.java
@@ -2,16 +2,17 @@ package memoEx.entity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Post {
     private int postLength = 0;                 // 게시글의 갯수
-    private PostEntity[] postEntities;          // final 삭제처리 해버림
+    private List<PostEntity> postEntities = new ArrayList<>();          // final 삭제처리 해버림
     private final int POST_SIZE = 20;           // 게시글의 최대 저장 갯수
 
-    public Post(){
-        this.postEntities = new PostEntity[POST_SIZE];
-    }
+//    public Post(){
+//        this.postEntities = new PostEntity[POST_SIZE];
+//    }
 
     public void write(){
         System.out.println("");
@@ -28,7 +29,8 @@ public class Post {
         System.out.println("글 내용을 작성해주세요.");
         String content = scanner.nextLine();
 
-        postEntities[this.postLength++] = PostEntity.newInstance(title, content);
+//        postEntities[this.postLength++] = PostEntity.newInstance(title, content);
+        postEntities.add(PostEntity.newInstance(title, content));
 
         System.out.println("게시글이 작성되었습니다.");
         System.out.println("");
@@ -36,14 +38,18 @@ public class Post {
 
     public void printAllPosts(){
         System.out.println("");
-        if( this.postLength == 0){
+//        if( this.postLength == 0){
+        if(postEntities.get(0).getTitle() == null){
             System.out.println("조회할 수 있는 게시글이 없습니다.");
             System.out.println("");
             return;
         }
 
-        for (int i = 0; i < this.postLength; i++){
-            PostEntity postEntity = postEntities[i];
+//        for (int i = 0; i < this.postLength; i++){
+        for(int i = 0; i < postEntities.size(); i++){
+//            PostEntity postEntity = postEntities[i];
+            PostEntity postEntity = postEntities.get(i);
+            System.out.println(postEntity);
 
             String headLine = String.format("게시글 번호: %d 게시글 제목: %s 작성일: %s", i, postEntity.getTitle(), postEntity.getLastUpdatedDateTime());
             System.out.println(headLine);
@@ -57,7 +63,8 @@ public class Post {
         Scanner scanner = new Scanner(System.in);
         System.out.println("조회할 게시글의 번호를 입력하세요");
         int selectedPost = scanner.nextInt();
-        PostEntity postEntity = postEntities[selectedPost];
+//        PostEntity postEntity = postEntities[selectedPost];
+        PostEntity postEntity = postEntities.get(selectedPost);
         if (postEntity == null){
             System.out.println("작성된 글이 없습니다.");
             System.out.println("");
@@ -81,7 +88,8 @@ public class Post {
         System.out.println("게시글을 작성해주세요");
         String content = scanner.nextLine();
 
-        PostEntity postEntity = postEntities[selectedPost];
+//        PostEntity postEntity = postEntities[selectedPost];
+        PostEntity postEntity = postEntities.get(selectedPost);
         if ( postEntity == null ){
             System.out.println("존재하지 않는 글입니다.");
             return;
@@ -99,10 +107,12 @@ public class Post {
         System.out.println("삭제할 게시글을 선택해주세요");
         int selectedPost = Integer.parseInt(scanner.nextLine());
 
-        ArrayList<PostEntity> postList = new ArrayList<>(Arrays.asList(postEntities));
-        postList.remove(selectedPost);
-        this.postEntities = new PostEntity[--postLength];
-        postEntities = postList.toArray(PostEntity[]::new);
+        postEntities.remove(selectedPost);
+
+//        ArrayList<PostEntity> postList = new ArrayList<>(Arrays.asList(postEntities));
+//        postList.remove(selectedPost);
+//        this.postEntities = new PostEntity[--postLength];
+//        postEntities = postList.toArray(PostEntity[]::new);
 
 
     }
